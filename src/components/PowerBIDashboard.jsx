1a4754ddcf3754aa879e7a96cc7a9edae184cea9
Mitul <mitul@riveredgeanalytics.com>
2025-07-01 13:00:37 +0530

    Update IFrame V3.3

--- src/components/PowerBIDashboard.jsx
+++ src/components/PowerBIDashboard.jsx
@@ -22,10 +22,10 @@ const PowerBIDashboard = ({ onBack }) => {
       <main className="powerbi-main">
         <div className="dashboard-wrapper">
         <iframe
-            title="AVDashboard_V3.2"
+            title="AVDashboard_V3.3"
             width="100%"
             height="100%"
-            src="https://app.powerbi.com/reportEmbed?reportId=8e45718d-172c-4261-b783-9f3b01276e4b&autoAuth=true&ctid=aa0b3340-f8f9-4cd3-910f-f15bb43eeb6a"
+            src="https://app.powerbi.com/reportEmbed?reportId=c56d34f3-cdc0-4f01-b5f2-ceedfa7922bb&autoAuth=true&ctid=aa0b3340-f8f9-4cd3-910f-f15bb43eeb6a"
             frameBorder="0"
             allowFullScreen="true"
             className="powerbi-iframe"
